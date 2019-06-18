430ae6c0a8eef68a78d66eb7889b315ff91b95ff
rbaraglia <baraglia.rudy@gmail.com>
2019-06-18 11:21:37 +0200

    Add architecture detection. Add usage prompt. Add target directory selection

--- setup.sh
+++ setup.sh
@@ -1,15 +1,54 @@
 #!/bin/bash
-TARGET_DIR=/home/pi
+
+usage () {
+	echo "Install LinTO modules"
+	echo "Usage is:"
+	echo "sudo ./setup.sh [--help | target_directory]"
+	echo "Default target directory is HOME"
+	exit 1
+}
+
+if [ "$1" = "--help" ]
+then
+	usage
+fi
+
+if [ -z "$1" ]
+then
+	TARGET_DIR=$HOME
+elif [ ! -d "$1" ]
+then
+	echo "ERROR: Specified directory does not exist"
+	exit 1
+else
+	TARGET_DIR="$1"
+fi
+
+echo "Target directory is $TARGET_DIR"
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [ "$USER" != "root" ]; then
-	echo "Script must be run as root"
+	echo "ERROR: Script must be run as root"
 	exit 1
 fi
 
 echo "The script will now install the client modules on the device ..."
 echo "Installing dependencies ..."
 sudo apt-get install -y portaudio19-dev libasound2-dev python3-pip libttspico-utils sox wget tar git
+
+if [ ! $ARCH ]
+then
+	ARCH="$(arch)"
+fi
+
+if [ $ARCH != "armv7l" ] && [ $ARCH != "x86_64" ]
+then
+	echo "ERROR: Supported architures are armv7l and x86_64 ($ARCH detected)"
+	exit 1
+fi
+echo "Detected architecture is $ARCH"
+export ARCH
 
 cd $TARGET_DIR
 # UI MODULE
@@ -26,13 +65,20 @@ git clone https://github.com/linto-ai/linto-models.git
 rm -rf model
 mv linto-models model
 
-echo "Copying services ..."
-cp $DIR/services/linto_ui.service /lib/systemd/system/
-cp $DIR/services/linto_tts.service /lib/systemd/system/
-cp $DIR/services/linto_command.service /lib/systemd/system/
+echo "SUCCESS: Modules have been successfully downloaded"
 
-echo "Enabling services ..."
-systemctl daemon-reload
-systemctl enable linto_ui linto_tts linto_command
+if  [ $ARCH = "armv7l" ]
+then
+	echo "Copying services ..."
+	cp $DIR/services/linto_ui.service /lib/systemd/system/
+	cp $DIR/services/linto_tts.service /lib/systemd/system/
+	cp $DIR/services/linto_command.service /libash get argumentsb/systemd/system/
 
-echo "... DONE (Reboot needed to start the modules)"
+	echo "Enabling services ..."
+	systemctl daemon-reload
+	systemctl enable linto_ui linto_tts linto_command
+
+	echo "... DONE (Reboot needed to start the modules)"
+fi
+
+
--- setup_command.sh
+++ setup_command.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ARCHIVE_URI=https://github.com/linto-ai/linto-command-module/releases/download/v0.1/linto-command-0.1-armv7l.tar.gz
+ARCHIVE_URI=https://github.com/linto-ai/linto-command-module/releases/download/v0.1/linto-command-0.1-$ARCH.tar.gz
 
 #Install last version
 echo "Downloading module COMMAND..."

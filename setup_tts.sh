1231a17aaf1e434451ad33d106937151efebba43
rbaraglia <rbaraglia@linagora.com>
2019-10-30 15:18:52 +0100

    Add package cleanup for pico

--- setup_tts.sh
+++ setup_tts.sh
@@ -11,3 +11,5 @@ rm $DEST/${ARCHIVE_URI##*/}
 wget http://ftp.us.debian.org/debian/pool/non-free/s/svox/libttspico0_1.0+git20130326-9_armhf.deb
 wget http://ftp.us.debian.org/debian/pool/non-free/s/svox/libttspico-utils_1.0+git20130326-9_armhf.deb
 apt-get install -fy ./libttspico0_1.0+git20130326-9_armhf.deb ./libttspico-utils_1.0+git20130326-9_armhf.deb
+
+rm http://ftp.us.debian.org/debian/pool/non-free/s/svox/libttspico0_1.0+git20130326-9_armhf.deb http://ftp.us.debian.org/debian/pool/non-free/s/svox/libttspico-utils_1.0+git20130326-9_armhf.deb

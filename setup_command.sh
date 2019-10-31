876a959f5dc12bc387144427cc128ac499169395
rbaraglia <rbaraglia@linagora.com>
2019-10-31 09:54:32 +0100

    Version change

--- setup_command.sh
+++ setup_command.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DEST=$1
-ARCHIVE_URI=https://github.com/linto-ai/linto-command-module/releases/download/v0.1/linto-command-0.1-$ARCH.tar.gz
+ARCHIVE_URI=https://github.com/linto-ai/linto-command-module/releases/download/v0.1/linto-command-0.2-$ARCH.tar.gz
 
 #Install last version
 echo "Downloading module COMMAND..."
